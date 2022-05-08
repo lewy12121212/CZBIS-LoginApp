d6fbd617065bf73c7956fb2f06b9dbe048ee1811
Eryk Lewandowski <lewy12121212@gmail.com>
2022-05-08 22:03:51 +0200

    Add logout method.

--- login-app/src/components/HomePage.js
+++ login-app/src/components/HomePage.js
@@ -2,8 +2,15 @@ import "./homePage.css";
 import "./global.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { Container, Navbar, Button } from "react-bootstrap";
+import { removeUserSession } from '../utils/session';
 
 export default function HomePage() {
+
+  const handleLogout = () => {
+    removeUserSession()
+    window.location = '/';
+  }
+
   return (
     <div className="app-main">
       <div className="shadow"></div>
@@ -12,7 +19,7 @@ export default function HomePage() {
           <Navbar bg="dark" expand="lg">
             <Container fluid>
               <Navbar.Brand href="#" style={{color: 'white'}}>Navbar</Navbar.Brand>
-              <Button variant="success">Wyloguj</Button>
+              <Button variant="success" onClick={handleLogout}>Wyloguj</Button>
             </Container>
           </Navbar>
           <div>
