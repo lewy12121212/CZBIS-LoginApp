b2512427b8e0f1f180f4ea4d74bdbb58449eb600
Lewy <lewy12121212@gmail.com>
2022-05-16 14:49:59 +0200

    FIX - rename components, change styles folder, remove unauthorize from routing.

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes, Outlet, Navigate } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import Page404 from './components/Page404';
-import Login from './components/Login';
-import Unautorize from './components/Unautorize';
+import ErrorPage from './components/ErrorPage';
+import LoginPage from './components/LoginPage';
 import { getToken } from './utils/session'
 
 function App() {
@@ -11,10 +10,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Outlet />}>
-          <Route path="" exac element={<Login />} />
+          <Route path="" exac element={<LoginPage />} />
           <Route exact path="HomePage" element={getToken() ? <HomePage /> : <Navigate to='/' replace />}/>
-          <Route path="Unautorize" element={<Unautorize />} />
-          <Route path="*" element={<Page404 />} />
+          <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
